fa55070b821575495605a46ef0b2d05bf2d5dbce
Jaime Rivas <j.rivas4818@gmail.com>
2022-04-15 10:10:10 -0700

    edit my index js file to start my questions to generate my html file

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const Employee = require('./lib/Employee')
+const Manager = require('./lib/Manager')
+const Intern = require('./lib/Intern')
+const Engineer = require('./lib/Engineer')
+
+
+// initial questions to generate the page
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the team manager?',
+        validate: nameInput => {
+            if (nameInput){
+                return true
+            }else{
+                console.log('Please enter the name of the team manager.')
+                return false;
+            }
+        }
+    }
+]
